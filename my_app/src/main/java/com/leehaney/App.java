c314c32584da0a3d7b8e5e003fad69a7112deaca
Lee <lhaney8891@gmail.com>
2021-03-29 00:10:17 -0400

    first commit

---
+++ my_app/src/main/java/com/leehaney/App.java
@@ -0,0 +1,9 @@
+package com.leehaney;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
